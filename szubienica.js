18bcf79e488b4d86cab08c93475fedb8363c7b1b
aniadewor <116655947+aniadewor@users.noreply.github.com>
2025-03-30 17:42:03 +0200

    create game

--- szubienica.js
+++ szubienica.js
@@ -3,6 +3,7 @@ haslo = haslo.toUpperCase();
 
 var dlugosc = haslo.length; 
 
+var ile_skuch = 0;
 var haslo1 = "";
 
 for (i=0; i<dlugosc; i++)
@@ -59,12 +60,60 @@ function start()
     var tresc_diva = "";
 
     for (i=0; i <=34; i++)
-    {
-        tresc_diva = tresc_diva + '<div class="litera">'+litery[i]+'</div>';
+    {   
+        var element = "lit" + i;
+        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>';
         if ((i+1) % 7 ==0 ) tresc_diva = tresc_diva +'<div style = "clear:both;"></div>'
     }
 
      document.getElementById("alfabet").innerHTML = tresc_diva;
 
      wypisz_haslo();
+}
+
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+  if (miejsce > this.length-1 )  return this.toString();
+  else return this.substring(0, miejsce)+ znak+ this.substring(miejsce+1);
+}
+
+function sprawdz(nr)
+{
+    var trafiona = false;
+    var element = "lit" + nr;
+
+    for(i=0; i<dlugosc; i++)
+    {
+        if (haslo.charAt(i)==litery[nr])
+        {
+            haslo1= haslo1.ustawZnak(i,litery[nr]);
+            trafiona = true;
+        }
+    }
+if (trafiona == true)
+{
+    document.getElementById(element).style.backgroundColor= "#003300";
+    document.getElementById(element).style.color = "#00C000";
+    document.getElementById(element).style.border = " 3px solid #003300";
+    document.getElementById(element).style.cursor = "default";
+    wypisz_haslo();
+}
+else
+{
+    document.getElementById(element).style.backgroundColor= "#330000";
+    document.getElementById(element).style.color = "#C00000";
+    document.getElementById(element).style.border = " 3px solid #C00000";
+    document.getElementById(element).style.cursor = "default";
+    document.getElementById(element).setAttribute("onclick",";");
+
+    ile_skuch++;
+    var obraz = "img/s" +ile_skuch + ".jpg";
+    document.getElementById("szubienica").innerHTML = '<img src= "'+obraz+'" alt="" />';
+}
+if (haslo == haslo1)
+document.getElementById("alfabet").innerHTML = "Podano prawidłowe hasło: "+ haslo+ '<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</SPAN>'
+
+if (ile_skuch>=9)
+    document.getElementById("alfabet").innerHTML = "Przegrana! Prawidłowe hasło: "+ haslo+ '<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</SPAN>'
+
 }
